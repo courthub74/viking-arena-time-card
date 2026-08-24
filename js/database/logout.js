3d76f7edc4e7b4d90239cae78ab2c0f7bb11a288
Courdevelops <courdevelops@gmail.com>
2026-08-24 13:37:59 -0400

    fixed logout redirect.

--- js/database/logout.js
+++ js/database/logout.js
@@ -27,6 +27,8 @@ const logoutManager = document.getElementById('logout_manager');
 
 // Log out function
 function logoutUser() {
+    // Test print the current user
+    console.log(`Logging out user: ${currentUser.username} and their role is: ${currentUser.accountType} session: ${currentUser.sessionId} from (logout.js)`);
     // Delete the session storage item
     sessionStorage.removeItem('currentUser');
 
@@ -35,8 +37,8 @@ function logoutUser() {
 
     // Redirect to login page
     window.location.href = '../../index.html';
-    window.location.href = '/viking_arena_time_card/index.html';
-
+    // Test Redirect to google.com for testing purposes
+    // window.location.href = 'https://www.google.com';
 }
 
 
